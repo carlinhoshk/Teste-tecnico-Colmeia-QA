199704de74d7dbba6c0784c23a148975e55a27c4
Carlos Oliveira <carlosmdohk@gmail.com>
2026-05-08 14:31:13 -0300

    Refactor Cypress CI workflow to restrict branch triggers and improve error handling

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotOnRunFailure: false,
   e2e: {
     baseUrl: 'https://teste-colmeia-qa.colmeia-corp.com',
   },
